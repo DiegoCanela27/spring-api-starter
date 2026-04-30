cf1aba9ba0eb2eb2a4d3ffeeb00ac04df53015d2
Diego Fernando <diego.gonzalezcnl@uanl.edu.mx>
2026-04-29 23:11:41 -0600

    Jakarta
    Se agregaron validaciones con Jakarta.

--- src/main/java/com/codewithmosh/store/dtos/RegisterUserRequest.java
+++ src/main/java/com/codewithmosh/store/dtos/RegisterUserRequest.java
@@ -1,10 +1,21 @@
 package com.codewithmosh.store.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class RegisterUserRequest {
+    @NotBlank(message = "Nombre es obligatorio")
+    @Size(max = 255, message = "Nombre debe tener menos de 255 caracteres")
     private String name;
+
+    @NotBlank(message = "Correo es obligatorio")
+    @Email(message = "El correo debe ser valido")
     private String email;
+
+    @NotBlank(message = "Contraseña es obligatoria")
+    @Size(min = 6, max = 25, message = "Contraseña debe ser entre 6 y 25 caracteres")
     private String password;
 }
