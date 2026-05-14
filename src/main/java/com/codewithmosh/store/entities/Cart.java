9b0209782052a6e8a1adcc2b28729e81e204426f
Diego Fernando <diego.gonzalezcnl@uanl.edu.mx>
2026-05-13 21:46:03 -0600

    Entidades
    Se crearon las entidades para el carrito.

---
+++ src/main/java/com/codewithmosh/store/entities/Cart.java
@@ -0,0 +1,27 @@
+package com.codewithmosh.store.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "carts")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "date_created", insertable = false, updatable = false)
+    private LocalDateTime dateCreated;
+
+    @OneToMany(mappedBy = "cart")
+    private Set<CartItem> cartItems = new LinkedHashSet<>();
+}
